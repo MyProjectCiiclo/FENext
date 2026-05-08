c79f714b1d63d0a56667f12d177401385f605c48
KimThanh1801 <hokimthanh1234@gmail.com>
2026-05-08 16:56:20 +0700

    rename Work.ts to work.ts

--- hooks/useWork.ts
+++ hooks/useWork.ts
@@ -1,5 +1,5 @@
 import { workService } from "@/services/work.service";
-import { WorkExperience } from "@/types/Work";
+import { WorkExperience } from "@/types/work";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
